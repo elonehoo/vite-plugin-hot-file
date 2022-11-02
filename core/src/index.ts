f84e3e4f19e624364a6ae1144948cc47e569bd53
elonehoo <huchengyea@163.com>
2022-11-02 15:08:51 +0800

    fix:initially created plugin content, and then used it

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,2 +1,72 @@
-export const one = 1
-export const two = 2
+import path from 'node:path'
+import { cwd } from 'process'
+import child_process from 'child_process'
+import type { Plugin } from 'vite'
+import { loadConfig } from 'unconfig'
+import chokidar from 'chokidar'
+import { main } from 'auto-export/utils'
+
+function HotFile(): Plugin {
+  return {
+    name:'hot-file',
+    apply: 'serve',
+    config(config,{ command }){
+      excuteAutoExport()
+      if(command === 'serve'){
+        loadConfig<ExportConfig>({
+          sources: [
+            {
+              files: 'export.config',
+              extensions: ['ts', 'mts', 'cts', 'js', 'mjs', 'cjs', 'json', ''],
+            },
+          ],
+        }).then(({config:configArray})=>{
+          const configs = configArray.configs
+          for (let i = 0; i < configs.length; i++){
+            const config = configs[i]
+            chokidar.watch(path.resolve(cwd(), config.targetDir), {
+              ignoreInitial: true,
+              atomic: true,
+              followSymlinks: true,
+            }).on('all', (event, pathDir) => {
+              if (!pathDir.includes('index')) {
+                excuteAutoExport()
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+}
+
+function excuteAutoExport() {
+  try {
+    main()
+  }
+  catch (error) {
+    child_process.execSync('npx autoexport')
+  }
+}
+
+type CustomImport = (fileName: string, file: string, fileType: string) => string
+
+interface Config {
+  targetDir: string
+  outputDir?: string
+  customImport?: CustomImport
+  depth?: boolean
+  autoPrefix?: boolean
+}
+
+export interface ExportConfig {
+  configs: Array<Config>
+}
+
+function defineExportConfig(config: ExportConfig): ExportConfig {
+  return config
+}
+
+export { defineExportConfig }
+
+export default HotFile
